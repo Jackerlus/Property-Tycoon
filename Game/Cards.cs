2c56ade2899c14a5339d59f5ab52757f886774ec
Thomas Senyah <51800322+Tsenyah@users.noreply.github.com>
2020-04-24 15:52:45 +0100

    Add files via upload

---
+++ Game/Cards.cs
@@ -0,0 +1,467 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace Property_Tycoon
+{
+    public class Cards
+    {
+		
+		CardType type;
+		String text;
+		Window1 pickOrDraw;
+		ArrayList opportunityKnocks = new ArrayList();
+        ArrayList potLuck = new ArrayList();
+		Board currentGame;
+		Random rand = new Random();
+		
+		public enum CardType { POT,OPP }
+		public Cards()
+		{
+			text = "";
+
+			for (int i = 0; i < 16; i++)
+			{
+				potLuck.Add(i);
+				opportunityKnocks.Add(i);
+
+			}
+			
+		}
+			
+
+		public void drawPotLuck(Player p)
+		{
+
+				int cardTaken = (int)potLuck[rand.Next(0, potLuck.Capacity)];
+				potLuck.TrimToSize();
+				pot(cardTaken, p);
+		}
+		public void drawOppKnocks(Player p)
+		{
+
+			int cardTaken = (int)potLuck[rand.Next(0, potLuck.Capacity)];
+			opportunityKnocks.TrimToSize();
+			oppKnocks(cardTaken, p);
+		}
+
+
+		private void pot(int a,Player p)
+		{
+			type = CardType.POT;
+			switch (a)
+			{
+				case 0:
+					inherit(p);
+					break;
+				case 1:
+					beauty(p);
+					break;
+				case 3:
+					moveToCrapper(p);
+					break;
+				case 4:
+					stdLoanRefund(p);
+					break;
+				case 5:
+					bankError(p);
+					break;
+				case 6:
+					payBillTextBook(p);
+					break;
+				case 7:
+					advanceToGo(p);
+					break;
+				case 8:
+					fineOrDrawOpp(p);
+					break;
+				case 9:
+					payInsurance(p);
+					break;
+				case 10:
+					collectSavingBond(p);
+					break;
+				case 11:
+					goToJail(p);
+					break;
+				case 12:
+					recievedInterest(p);
+					break;
+				case 13:
+					birthday();
+					break;
+				case 14:
+					getOutofJailFree(p);
+					break;
+				case 16:
+					bitcoin(p);
+					break;
+
+				default:
+					break;
+			}
+		}
+		private void bitcoin(Player p)
+		{
+			
+			text = "From sale of bitcoin"
+			+ "you get "
+			+ "£50";
+			
+			p.addmoney(50);
+			MessageBox.Show(text);
+		}
+
+		private void getOutofJailFree(Player p)
+		{
+			p.addGetOutOfJail();
+			text = "you have been given a\n"
+			+ "get out of jail free card \n"
+			+ "this has no resale or trade value";
+			MessageBox.Show(text);
+			
+		}
+
+		private void birthday()
+		{
+			
+			text = "It's Your birthday"
+			+ "from each player "
+			+ "Collect £10";
+			MessageBox.Show(text);
+
+		}
+
+		private void goToJail(Player p)
+		{
+			
+			text = "Go to Jail. \n"
+			+ "Do not pass Go. \n"
+			+ "Do not collect £200.";
+			MessageBox.Show(text);
+			p.goToJail();
+			
+		}
+
+		private void recievedInterest(Player p)
+		{
+			
+			text = "Recived intrest on"
+			 + "shares.\n "
+			 +"Collect £25";
+			p.addmoney(25);
+			MessageBox.Show(text);
+		}
+
+		private void collectSavingBond(Player P)
+		{
+			P.addmoney(100);
+			
+			text= "Savings bond matures. \n"
+			+ "collect £100 ";
+
+			MessageBox.Show(text);
+		}
+
+		private void payInsurance(Player p)
+		{
+			p.addmoney(-50);
+			
+			text = "Pay insurance fee of "+
+			"£50 ";
+			MessageBox.Show(text);
+		}
+
+		private void fineOrDrawOpp(Player p)
+		{
+			text = "Pay a $10 fine "
+			+ "or "
+			+ "take opportunity knocks";
+			pickOrDraw = new Window1();
+			pickOrDraw.data(text);
+			pickOrDraw.Show();
+
+			int choice = pickOrDraw.getChoice();
+
+			switch (choice) {
+				case 0:
+					// this pays the fine.
+					p.addmoney(-10);
+					break;
+				case 1:
+					// this draws a card.
+					drawOppKnocks(p);
+					break;
+				default:
+					break;
+			}
+
+		}
+
+		private void advanceToGo(Player p)
+		{
+
+			text= "Advance to go.";
+
+			p.moveto(0);
+			MessageBox.Show(text);
+		}
+
+		private void payBillTextBook(Player p)
+		{
+
+			text = "pay bill for textbooks "
+			+ "£100";
+			p.addmoney( -100);
+			MessageBox.Show(text);
+		}
+
+		private void bankError(Player p)
+		{
+
+			text = "bank error in "
+			+ "your favour. \n "
+			+"Collect £200";
+			p.addmoney( 200);
+			MessageBox.Show(text);
+		}
+
+		private void stdLoanRefund(Player p)
+		{
+
+			text = "Student Loan refund. \n"
+			
+			+ "Collect £20";
+			p.addmoney(20);
+			MessageBox.Show(text);
+		}
+
+		private void inherit(Player p)
+		{
+
+			text = "You have inherited £50! \n ";
+			
+			p.addmoney(10);
+			MessageBox.Show(text);
+		}
+		private void moveToCrapper(Player p)
+		{
+			
+			
+			
+			text = "Go Back to crapper Street";
+
+			MessageBox.Show(text);
+			p.moveto(2);
+		}
+		private void beauty(Player p)
+		{
+			p.addmoney(10);
+			text = "You have won second prize in a \n"
+			+"beauty contest!\n"
+			+"Collect £10";
+
+			MessageBox.Show(text);
+		}
+	
+
+	private void oppKnocks(int a, Player p)
+	{
+		type = Cards.CardType.OPP;
+		switch (a)
+		{
+			case 0:
+				divided(p);
+				break;
+			case 1:
+				lipsync(p);
+				break;
+			case 3:
+				moveToTuringHeights(p);
+				break;
+			case 4:
+				moveToHanXinGardens(p);
+				break;
+			case 5:
+				fine(p);
+				break;
+			case 6:
+				uniFees(p);
+				break;
+			case 7:
+				tripToHove(p);
+				break;
+			case 8:
+				loanMatures(p);
+				break;
+			case 9:
+				repairs(p);
+				break;
+			case 10:
+				advanceToGo(p);
+				break;
+			case 11:
+				repairsLess(p);
+				break;
+			case 12:
+				goBack(p);
+				break;
+			case 13:
+				advanceToSkywalker(p);
+				break;
+			case 14:
+				goToJail(p);
+				break;
+			case 16:
+				drunkincharge(p);
+				break;
+
+			default:
+				break;
+		}
+	}
+
+		private void drunkincharge(Player p)
+		{
+			p.addmoney(-20);
+
+			text = "Drunk in charge of a skateboard. Fine £20";
+
+			MessageBox.Show(text);
+		}
+
+		private void advanceToSkywalker(Player p)
+		{
+			
+
+			text = "Advance to Skywalker Drive. If you pass GO collect £200";
+
+			MessageBox.Show(text);
+			p.moveto(12);
+		}
+
+		private void goBack(Player p)
+		{
+			
+
+			text = "Go back 3 spaces";
+
+			MessageBox.Show(text);
+			p.move(-3);
+		}
+
+		private void repairsLess(Player p)
+		{
+			int houseCounter = 0;
+			int hotelCounter = 0;
+			foreach (Property item in p.getProperties())
+			{
+				houseCounter = houseCounter + item.checkHouses();
+				if (item.hotelCheck())
+				{
+					hotelCounter = hotelCounter + 1;
+				}
+	
+			}
+			p.addmoney(-((houseCounter * 25 )+ (hotelCounter * 100)));
+			text = "You are assessed for repairs, £25/house, £100/hotel";
+
+			MessageBox.Show(text);
+		}
+
+		private void repairs(Player p)
+		{
+			int houseCounter = 0;
+			int hotelCounter = 0;
+			foreach (Property item in p.getProperties())
+			{
+				houseCounter = houseCounter + item.checkHouses();
+				if (item.hotelCheck())
+				{
+					hotelCounter = hotelCounter + 1;
+				}
+
+			}
+			
+
+			text = "You are assessed for repairs, £40/house, £115/hotel";
+
+			MessageBox.Show(text);
+			p.addmoney(-((houseCounter * 40) + (hotelCounter * 115)));
+		}
+
+		private void loanMatures(Player p)
+		{
+			p.addmoney(150);
+
+			text = "Loan matures, collect £150";
+
+			MessageBox.Show(text);
+		}
+
+		private void tripToHove(Player p)
+		{
+			p.moveto(16);
+
+			text = "Take a trip to Hove station. If you pass GO collect £200";
+
+			MessageBox.Show(text);
+		}
+
+		private void uniFees(Player p)
+		{
+			p.addmoney(-150);
+
+			text = "Pay university fees of £150";
+
+			MessageBox.Show(text);
+		}
+
+		private void fine(Player p)
+		{
+			p.addmoney(-15);
+
+			text = "Fined £15 for speeding";
+
+			MessageBox.Show(text); 
+		}
+
+		private void moveToHanXinGardens(Player p)
+		{
+			p.moveto(25);
+
+			text = "Advance to Turing Heights";
+
+			MessageBox.Show(text);
+		}
+
+		private void moveToTuringHeights(Player p)
+		{
+			p.moveto(40);
+
+			text = "Advance to Turing Heights";
+
+			MessageBox.Show(text);
+		}
+
+		private void lipsync(Player p)
+		{
+			p.addmoney(100);
+
+			text = "You have won a lip sync battle. Collect £100";
+
+			MessageBox.Show(text);
+		}
+
+		private void divided(Player p)
+		{
+			p.addmoney(50);
+
+			text = "Bank pays you a divided of £50";
+
+			MessageBox.Show(text);
+		}
+	}
+}
---
+++ Game/Human.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace Property_Tycoon
+{
+    class Human : Player
+    {
+        private int JAIL = 31;
+        private int VISITING = 11;
+        private int GO = 0;
+        private int FREE = 5;
+        private int Pot1 = 3;
+        private int Pot2 = 34;
+
+
+        private ArrayList properties;
+        private String playerName;
+        private int money;
+        private int position;
+        private bool inJail;
+        private int jailTurn;
+        private int playerNumber;
+        private bool justVisiting;
+        private bool rollBan;
+        private int numJailFree;
+        private Roll roll;
+        private int rollcounter;
+        private int goTile;
+        private Board CurrentGame;
+
+        public Human(string name, int money, int position, int playerno, Board cg)
+        {
+            
+            this.money = money;
+            this.playerName = name;
+            this.jailTurn = 0;
+            this.numJailFree = 0;
+            this.inJail = false;
+            this.position = position;
+            this.properties = new ArrayList();
+            rollcounter = 0;
+            roll = new Roll();
+            rollBan = false;
+            CurrentGame = cg;
+            
+        }
+
+        
+
+        /// <summary>
+        /// this method adds to the get out of jail free cards part
+        /// </summary>
+        /// <param name="val"></param>
+        public void addGetOutofJail(int val)
+        {
+            numJailFree++;
+        }
+
+        /// <summary>
+        /// this method adds money to the players account
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public int addmoney(int val)
+        {
+            if (!inJail)
+            {
+                money = money + val;
+                
+            }
+            return money;
+
+        }
+        /// <summary>
+        /// this method allows the user to purchase a property
+        /// </summary>
+        /// <param name="p"></param>
+        public String buyProperty(Property p)
+        {
+            String s = "";
+            if (p.isBankOwned())
+            {
+                s = p.getName()+"aquired";
+                money = money - p.getCost();
+                p.setBankOwned(false);
+                p.SetOwner(this);
+                properties.Add(p);
+            }
+
+            else if (p.getPlayer() == this)
+            {
+                s = "you already own this";
+            }
+
+            else {
+                s = "this property is owned by" + p.getName();
+            }
+      
+            return s;
+        }
+
+        public void drawcard() {
+            
+        }
+
+        public int getJailFreeNo()
+        {
+            return numJailFree;
+        }
+
+        public int getJailTurns()
+        {
+            return jailTurn;
+        }
+        /// <summary>
+        /// returns the amount of money the player has
+        /// </summary>
+        /// <returns></returns>
+        public int getMoney()
+        {
+            return money;
+        }
+        /// <summary>
+        /// this method calls the GO tiles action method
+        /// </summary>
+        /// <param name="goSpace"></param>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public String goSpace(Go goSpace,Player p) {
+            goSpace.action(p);
+            return "you have recieved 200";
+        }
+        /// <summary>
+        /// returns player name
+        /// </summary>
+        /// <returns></returns>
+        public string getName()
+        {
+            return playerName;
+        }
+        /// <summary>
+        /// returns property list
+        /// </summary>
+        /// <returns></returns>
+        public ArrayList getProperties()
+        {
+            return properties;
+        }
+        /// <summary>
+        /// method that simulates jail
+        /// </summary>
+        public void goToJail()
+        {
+            inJail = true;
+            jailTurn = 0;
+            position = VISITING;
+        }
+        /// <summary>
+        /// method that simulates leaving jail
+        /// </summary>
+        public void leaveJail()
+        {
+            money = money - 50;
+            jailTurn = 0;
+            inJail = false;
+        }
+        /// <summary>
+        /// method to mortgage a property
+        /// </summary>
+        /// <param name="p"></param>
+        public void MortgageProperty(Property p)
+        {
+            if (this.properties.Contains(p))
+            {
+                money = money + (p.getCost() / 2);
+                p.mortgageProperty();
+            }
+        }
+        /// <summary>
+        /// method to move a player around the board
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public int move(int val)
+        {
+                if (!inJail)
+                {
+                    position = position + val;
+                    if (position > 40 && position != JAIL)
+                    {
+                        position = position % 40;
+                        
+                    }
+                    if (position == JAIL) {
+                        goToJail();
+
+                    }
+                    if (position == GO)
+                    {
+                    CurrentGame.GoTile.action(this);
+                    }
+                    if (position == FREE)
+                    {
+                    CurrentGame.justVising.action(this);
+                    }
+                    if (position == Pot1 || position == Pot2) {
+                        MessageBox.Show(" PotLuck");
+                        CurrentGame.potLuck.action(this);
+                    }
+                    if (position == 8 || position == 23)
+                    {
+                        MessageBox.Show(" Opportunity knocks");
+                        CurrentGame.opportunities.action(this);
+                    }
+            }
+            
+            
+            return position;
+        }
+        /// <summary>
+        /// methood to gget the value of the dice roll
+        /// </summary>
+        /// <returns></returns>
+
+        public int getRollValue()
+        {
+            return roll.rollDice();
+        }
+        /// <summary>
+        /// this method rolls the dice classes and returns a result. unless there is a double the user cannot roll again. if there are 
+        /// too many doubles in a row then the current player is sent to jail
+        /// </summary>
+        /// <returns></returns>
+        public String rolldice()
+        {
+            String s = "";
+            if (rollBan == false)
+            {
+                roll.Rolls();
+                
+                if (!inJail)
+                {
+
+                    move(roll.Rolls());
+                    if (roll.isEqual(roll.getDie1(), roll.getDie2()))
+                    {
+                        s = "Double "+roll.getDie1()+"!! roll again ";
+                        move(getRollValue());
+                        rollBan = false;
+                        return s;
+                    }
+
+                    if (roll.getDoubleCounter() == 3)
+                    {
+                        goToJail();
+                        s = "third double to jail you go";
+                        rollBan = true;
+                    }
+                    else {
+                        rollBan = true;
+                    }
+                    
+                }
+
+                else if (getPosition() == JAIL && inJail && jailTurn < 3)
+                {
+                    jailTurn++;
+                    s = "No double. you remain in jail";
+                    rollBan = true;
+                }
+                else if (getPosition() == JAIL && inJail && jailTurn == 3)
+                {
+                    leaveJail();
+
+                    s = "youve paid 50 and have left jail";
+                    addmoney(-50);
+                    move(getRollValue());
+                    rollBan = true;
+                }
+
+            }
+            else {
+                s = "you have previously rolled. end turn";
+                }
+            MessageBox.Show(s);
+            return s;
+
+        }
+
+        public int moveto(int pos)
+        {
+
+            position = pos;
+            MessageBox.Show("this is the new position" + position);
+            return position;
+        }
+        public void addGetOutOfJail() {
+            numJailFree++;
+        }
+        /// <summary>
+        /// method that uses a get of jail free card;
+        /// </summary>
+        /// <returns></returns>
+        public String useGetOutofJail()
+        {
+            String s = "";
+            if (numJailFree > 0)
+            {
+                jailTurn = 0;
+                inJail = false;
+                numJailFree--;
+                s = ("you have " + numJailFree + " get out of free jail cards left");
+
+            }
+            else {
+                s = "You are out of jail";
+            }
+            return s;
+        }
+        /// <summary>
+        /// method to get the current position of a player
+        /// </summary>
+        /// <returns></returns>
+        public int getPosition() { return position; }
+        /// <summary>
+        /// method that ends the players turn
+        /// </summary>
+        public void endTurn() {
+            rollBan = false;
+        }
+        /// <summary>
+        ///  method to check if the player is in jail
+        /// </summary>
+        /// <returns></returns>
+        public bool isInJail() {
+            return inJail;
+        }
+
+        public Player getPlayer()
+        {
+            return this;
+        }
+        /*
+        public String checkRent() {
+            if (this.position == pr)
+            {
+
+            } 
+        }*/
+    }
+    
+}
